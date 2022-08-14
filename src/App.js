bde123fd44225549b88395ecf4b357c7a6961781
Cong Liu <cong.liu@ip-192-168-6-10.ap-northeast-1.compute.internal>
2022-08-14 20:17:09 +0800

    fix img

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import "./App.css";
-import Home from "./Home.js";
-import Nft from "./Nft.js";
-import WhitePaper from "./WhitePaper";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/whitePaper" element={<WhitePaper />} />
-        <Route path="/nft" element={<Nft />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>App</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Home from "./Home.js";
+import Nft from "./Nft.js";
+import WhitePaper from "./WhitePaper";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/whitePaper" element={<WhitePaper />} />
+      <Route path="/nft" element={<Nft />} />
+    </Routes>
+  </BrowserRouter>
+);
