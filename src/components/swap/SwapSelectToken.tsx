01970e1074a562aa3ed9576c1fe7d665c88dc57c
Cong Liu <cong.liu@ip-192-168-6-11.ap-northeast-1.compute.internal>
2022-08-17 22:58:16 +0800

    fix react app

--- src/components/swap/SwapSelectToken.tsx
+++ src/components/swap/SwapSelectToken.tsx
@@ -1,18 +1,25 @@
-import { Modal } from 'antd';
-import React from 'react';
-import { useAppSelector, useAppDispatch } from '../../store/Hook';
-import {setInVisiable} from "../../store/swap/TokenSelect"
+import { Modal } from "antd";
+import React from "react";
+import { useAppSelector, useAppDispatch } from "../../store/Hook";
+import { setInVisiable } from "../../store/swap/TokenSelect";
 
-function SwapSelectToken (){
-    const visibility = useAppSelector(state => state.tokenSelect.value);
-    const dispatch = useAppDispatch();
-    return (
-        <Modal title="Select a Token" visible={visibility}  footer={null} destroyOnClose={true} closable={true} onCancel={()=>{
-                  dispatch(setInVisiable());
-               }}>
-        </Modal>
-        
-    )
+function SwapSelectToken() {
+  const visibility = useAppSelector((state) => state.tokenSelect.value);
+  const dispatch = useAppDispatch();
+  return (
+    <Modal
+      title="Select a Token"
+      visible={visibility}
+      footer={null}
+      destroyOnClose={true}
+      closable={true}
+      onCancel={() => {
+        dispatch(setInVisiable());
+      }}
+    >
+      <div></div>
+    </Modal>
+  );
 }
 
 export default SwapSelectToken;
