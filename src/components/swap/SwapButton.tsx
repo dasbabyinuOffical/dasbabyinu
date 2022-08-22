9dfef7109537cc651768e539521ba49483d9e27c
Cong Liu <cong.liu@ip-192-168-6-18.ap-northeast-1.compute.internal>
2022-08-22 18:33:36 +0800

    fix button amount

--- src/components/swap/SwapButton.tsx
+++ src/components/swap/SwapButton.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import { Button } from 'antd';
+import React, { useEffect, useState } from "react";
+import { Button } from "antd";
+import { useAppSelector } from "../../store/Hook";
 
-const SwapButton: React.FC = () => (
+function SwapButton() {
+  const inputToken = useAppSelector((state) => state.tokenSelect.inputToken);
+  const outputToken = useAppSelector((state) => state.tokenSelect.outputToken);
+
+  const [status, setStatus] = useState(false);
+
+  useEffect(() => {
+    if (inputToken.value === "" || Number(inputToken.value) == 0) {
+      setStatus(true);
+      return;
+    }
+    if (outputToken.value === "" || Number(outputToken.value) == 0) {
+      setStatus(true);
+      return;
+    }
+    setStatus(false);
+  }, [inputToken, outputToken]);
+  return (
     <div className="swap-button">
-        <Button type='primary' shape='round' size='large' disabled={true}>Enter an amount</Button>
+      <Button type="primary" shape="round" size="large" disabled={status}>
+        Enter an amount
+      </Button>
     </div>
-)
+  );
+}
 
 export default SwapButton;
