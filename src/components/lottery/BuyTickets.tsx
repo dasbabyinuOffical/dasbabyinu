35edb7b0a510adde704f7bc6da5c54f5b52a3a54
liucong <cong.liu@ascendex.io>
2022-09-06 12:23:58 +0800

    add ticket

--- src/components/lottery/BuyTickets.tsx
+++ src/components/lottery/BuyTickets.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { Button, Statistic } from "antd";
+
+let starSmall = require("../../images/star-small.png");
+let startBig = require("../../images/star-big.png");
+let threeStar = require("../../images/three-stars.png");
+let ticketLeft = require("../../images/ticket-left.png");
+let ticketRight = require("../../images/ticket-right.png");
+let buyTickets = require("../../images/buyTickets.svg");
 
 function BuyTickets() {
-  return <div>Buy Tickets</div>;
+  return (
+    <div className="buyTickets">
+      <div className="buyTickets-imgs">
+        <img src={startBig} className="big-star-img" />
+        <img src={starSmall} className="small-star-img" />
+        <img src={threeStar} className="three-star-img" />
+        <img src={ticketLeft} className="ticket-left-img" />
+        <img src={ticketRight} className="ticket-right-img" />
+        <div className="buyTickets-title">The DasBabySwap Lottery </div>
+        <div className="buyTickets-money">
+          <Statistic
+            value={116163}
+            prefix="$"
+            valueStyle={{ color: "rgb(253, 171, 50)" }}
+          />
+        </div>
+        <div className="buyTickets-prizes">in prizes! </div>
+        <div className="buyTickets-button">
+          <button className="buyTickets-btn">BuyTickets</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BuyTickets;
