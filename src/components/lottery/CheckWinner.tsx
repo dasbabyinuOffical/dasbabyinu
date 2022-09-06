e4c37f28a64ea759e46f5411715ed1e335833c21
liucong <cong.liu@ascendex.io>
2022-09-06 17:48:09 +0800

    add check winner

--- src/components/lottery/CheckWinner.tsx
+++ src/components/lottery/CheckWinner.tsx
@@ -1,7 +1,30 @@
+import { Button, Space } from "antd";
 import React from "react";
 
+let ticketRight = require("../../images/ticket-left.png");
+let ticketLeft = require("../../images/ticket-right.png");
+
 function CheckWinner() {
-  return <div>Check Winner</div>;
+  return (
+    <div className="buyTickets-winner">
+      <div>
+        <img src={ticketLeft} />
+      </div>
+      <div>
+        <Space direction="vertical">
+          <div className="buyTickets-winner-ask">Are you a Winner?</div>
+          <div>
+            <Button type="primary" shape="round" size="large">
+              Check Now
+            </Button>
+          </div>
+        </Space>
+      </div>
+      <div>
+        <img src={ticketRight} />
+      </div>
+    </div>
+  );
 }
 
 export default CheckWinner;
