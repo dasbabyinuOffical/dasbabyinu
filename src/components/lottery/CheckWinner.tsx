2fc50022402182533d57e91cf77181d3f48f0f63
liucong <cong.liu@ascendex.io>
2022-09-10 20:33:31 +0800

    almost finish

--- src/components/lottery/CheckWinner.tsx
+++ src/components/lottery/CheckWinner.tsx
@@ -1,10 +1,31 @@
 import { Button, Space } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { userReward, status } from "../../util/lottery";
 
 let ticketRight = require("../../images/ticket-left.png");
 let ticketLeft = require("../../images/ticket-right.png");
+const daiAddress = "0x5eC5a89BDdF7AF48392B2f8a5419080470Ee238b";
 
 function CheckWinner() {
+  const [userRewards, setUserRewards] = useState<string>("0");
+  const [step, setStep] = useState<string>("-1");
+  useEffect(() => {
+    const rewardInterval = setInterval(() => {
+      // get reward
+      const account = localStorage.getItem("account");
+      userReward(daiAddress, account!).then((res) => {
+        setUserRewards(res);
+      });
+
+      // get status
+      status(daiAddress).then((res) => {
+        setStep(res);
+      });
+    }, 10000);
+    return () => {
+      clearInterval(rewardInterval);
+    };
+  }, []);
   return (
     <div className="buyTickets-winner">
       <div>
@@ -12,9 +33,14 @@ function CheckWinner() {
       </div>
       <div>
         <Space direction="vertical">
-          <div className="buyTickets-winner-ask">Claim My Rewards</div>
+          <div className="buyTickets-winner-ask">Rewards:${userRewards}</div>
           <div>
-            <Button type="primary" shape="round" size="large">
+            <Button
+              type="primary"
+              shape="round"
+              size="large"
+              disabled={step !== "2"}
+            >
               Claim Now
             </Button>
           </div>
--- src/util/lottery.tsx
+++ src/util/lottery.tsx
@@ -45,6 +45,21 @@ export async function totalReward(daiAddress: string): Promise<string> {
   return ret;
 }
 
+export async function userReward(
+  daiAddress: string,
+  address: string
+): Promise<string> {
+  if (!window.ethereum) {
+    return "0";
+  }
+
+  const providerWeb3 = new ethers.providers.Web3Provider(window.ethereum);
+  const daiContract = new ethers.Contract(daiAddress, LotteryAbi, providerWeb3);
+  const result = await daiContract.userRewards(address);
+  const ret = ethers.utils.formatUnits(result, 18);
+  return ret;
+}
+
 export async function status(daiAddress: string): Promise<string> {
   if (!window.ethereum) {
     return "0";
